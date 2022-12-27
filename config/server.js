5a673af6cd94a0ea7ea88fceb1bf21f22c868ab5
Ester Faranny <53008121+EsterFaranny@users.noreply.github.com>
2022-12-27 18:06:50 -0300

    Adicionando estilização e jquery

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
-var expressValidator = require('express-validator')
-var app = express()
+var expressValidator = require('express-validator');
+var app = express();
 
+app.use(express.static('./app/public'));
 app.set('view engine','ejs');
 app.set('views','./app/views');
 
